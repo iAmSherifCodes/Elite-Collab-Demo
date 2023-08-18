4ec27f66cfdd1bd08a60183357f0dff613d18bc7
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-18 08:36:07 +0100

    added odogwu

---
+++ src/main/java/org/example/collabTest/CollabTest.java
@@ -0,0 +1,9 @@
+package org.example.collabTest;
+
+import java.sql.SQLOutput;
+
+public class CollabTest {
+    public static void main(String[] args) {
+        System.out.println("Odogwu Legends");
+    }
+}
